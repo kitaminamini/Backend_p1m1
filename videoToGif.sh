bd42c08668f1d157934b861c30f6e1540c296984
Samart Furuta <tarofuruta@gmail.com>
2018-09-26 17:22:34 +0700

    adjusted frame size

--- videoToGif.sh
+++ videoToGif.sh
@@ -6,7 +6,7 @@ start=$3
 #echo $inputFile
 
 mkdir frames
-ffmpeg -y -ss $start -t 7 -i $inputFile -r 20 ./frames/ffout%03d.png
+ffmpeg -y -ss $start -t 7 -i $inputFile -vf scale=640:480 -r 20 ./frames/ffout%03d.png
 convert -delay 5 -loop 0 ./frames/ffout*.png $outputFile
 rm -rf frames
 
